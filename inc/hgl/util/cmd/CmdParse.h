23a4ee21a1f0b839c211010ad9108a9ae287af58
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-10-26 01:47:22 +0800

    CmdParse增加Contains函数,Find查找改为不区分大小写

--- inc/hgl/util/cmd/CmdParse.h
+++ inc/hgl/util/cmd/CmdParse.h
@@ -1,29 +1,27 @@
-﻿#ifndef HGL_COMMAND_LINE_PARSE_INCLUDE
-#define HGL_COMMAND_LINE_PARSE_INCLUDE
+﻿#pragma once
 
 #include<hgl/type/StringList.h>
-namespace hgl
+
+namespace hgl::util
 {
-    namespace util
+    /**
+    * 命令行参数解析辅助类
+    */
+    class CmdParse                                                          ///命令行参数解析辅助类
     {
-        /**
-        * 命令行参数解析辅助类
-        */
-        class CmdParse                                                          ///命令行参数解析辅助类
-        {
-            OSStringList args;
+        OSStringList args;
 
-        public:
+    public:
 
-            CmdParse(const OSStringList &);
-            CmdParse(const int,os_char **);
-            virtual ~CmdParse()=default;
+        CmdParse(const OSStringList &);
+        CmdParse(const int,os_char **);
+        virtual ~CmdParse()=default;
 
-            int Find(const OSString &)const;                                    ///<查找一个指定字串开头的参数是否存在
+        int Find(const OSString &)const;                                    ///<查找一个指定字串开头的参数是否存在
 
-            bool GetInteger(const OSString &,int *)const;                       ///<取得一个数值参数
-            bool GetString(const OSString &,OSString &)const;                   ///<取得一个字符串参数
-        };//class CmdParse
-    }//namespace util
-}//namespace hgl
-#endif//HGL_COMMAND_LINE_PARSE_INCLUDE
+        bool Contains(const OSString &flag)const{return Find(flag)!=-1;}    ///<检测是否包含某参数
+
+        bool GetInteger(const OSString &,int *)const;                       ///<取得一个数值参数
+        bool GetString(const OSString &,OSString &)const;                   ///<取得一个字符串参数
+    };//class CmdParse
+}//namespace hgl::util
--- src/cmd/CmdParse.cpp
+++ src/cmd/CmdParse.cpp
@@ -1,79 +1,76 @@
 ﻿#include<hgl/util/cmd/CmdParse.h>
 #include<hgl/type/StrChar.h>
 
-namespace hgl
+namespace hgl::util
 {
-    namespace util
+    CmdParse::CmdParse(const OSStringList &_args)
     {
-        CmdParse::CmdParse(const OSStringList &_args)
-        {
-            args=_args;
-        }
+        args=_args;
+    }
 
-        CmdParse::CmdParse(const int argc,os_char **argv)
-        {
-            for(int i=0;i<argc;i++)
-                args.Add(argv[i]);
-        }
+    CmdParse::CmdParse(const int argc,os_char **argv)
+    {
+        for(int i=0;i<argc;i++)
+            args.Add(argv[i]);
+    }
         
-        int CmdParse::Find(const OSString &flag)const
+    int CmdParse::Find(const OSString &flag)const
+    {
+        return args.CaseFind(flag,flag.Length());
+    }
+
+    bool CmdParse::GetInteger(const OSString &flag,int *result)const
+    {
+        int index=Find(flag);
+
+        if(index==-1)return(false);
+
+        const OSString &str=args[index];
+
+        if(str.Length()>flag.Length())
         {
-            return args.Find(flag,flag.Length());
+            stoi(str.c_str()+flag.Length(),*result);
+        }
+        else
+        {
+            stoi(str.c_str(),*result);
         }
 
-        bool CmdParse::GetInteger(const OSString &flag,int *result)const
+        return(true);
+    }
+
+    bool CmdParse::GetString(const OSString &flag,OSString &result)const
+    {
+        int index=Find(flag);
+        os_char *p=nullptr;
+
+        if(index==-1)return(false);
+
+        const OSString &str=args[index];
+
+        if(str.Length()>flag.Length())
         {
-            int index=Find(flag);
+            p=str.c_str()+flag.Length();
+        }
+        else
+        {
+            p=args[index+1];
+        }
 
-            if(index==-1)return(false);
+        if(*p=='"')    //有引号
+        {
+            int len=strchr(p+1,L'"')-p;
 
-            const OSString &str=args[index];
-
-            if(str.Length()>flag.Length())
-            {
-                stoi(str.c_str()+flag.Length(),*result);
-            }
+            if(len>1)
+                result.fromString(p+1,len-1);
             else
-            {
-                stoi(str.c_str(),*result);
-            }
-
-            return(true);
+                result=p+1;
         }
-
-        bool CmdParse::GetString(const OSString &flag,OSString &result)const
+        else            //无引号
         {
-            int index=Find(flag);
-            os_char *p=nullptr;
-
-            if(index==-1)return(false);
-
-            const OSString &str=args[index];
-
-            if(str.Length()>flag.Length())
-            {
-                p=str.c_str()+flag.Length();
-            }
-            else
-            {
-                p=args[index+1];
-            }
-
-            if(*p=='"')    //有引号
-            {
-                int len=strchr(p+1,L'"')-p;
-
-                if(len>1)
-                    result.fromString(p+1,len-1);
-                else
-                    result=p+1;
-            }
-            else            //无引号
-            {
-                result=p;
-            }
-
-            return(true);
+            result=p;
         }
-    }//namespace util
-}//namespace hgl
+
+        return(true);
+    }
+}//namespace hgl::util
