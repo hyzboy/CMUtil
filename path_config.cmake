33bb2723c914ec78a7c52438e9c7e755f80160cb
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-01-28 20:59:39 +0800

    改进cmake路径配置

--- path_config.cmake
+++ path_config.cmake
@@ -1,11 +1,9 @@
-macro(CMUtilSetup source_path)
+macro(CMUtilSetup CMUTIL_ROOT_PATH)
+    message("CMUtil root path: " ${CMUTIL_ROOT_PATH})
 
-    message("CMUTIL_ROOT_PATH: " ${source_path})
-
-    set(CMUTIL_ROOT_INCLUDE_PATH    ${source_path}/inc)
-    set(CMUTIL_ROOT_SOURCE_PATH     ${source_path}/src)
-
-    set(CMUTIL_ROOT_3RDPTY_PATH     ${source_path}/3rdpty)
+    set(CMUTIL_ROOT_INCLUDE_PATH ${CMUTIL_ROOT_PATH}/inc)
+    set(CMUTIL_ROOT_SOURCE_PATH ${CMUTIL_ROOT_PATH}/src)
+    set(CMUTIL_ROOT_3RDPTY_PATH ${CMUTIL_ROOT_PATH}/3rdpty)
 
     include_directories(${CMUTIL_ROOT_INCLUDE_PATH})
 endmacro()
